90c5568e292e0649d930a3b59a216580198afd17
森祐一朗 <apotikisin4632@gmail.com>
2025-01-29 16:41:11 +0900

    profile ページ　作成

--- frontend/fe/src/app/types/favorite.d.ts
+++ frontend/fe/src/app/types/favorite.d.ts
@@ -1,7 +1,10 @@
+import { TweetType } from "./tweet";
+
 export type FavoriteType = {
     id: number;
     userId: number;
     tweetId: number;
     createdAt: string;
     updatedAt: string;
+    tweet: TweetType;
 }
